32d9cb4a15bb46a017e2cf84ba2f97b3f51f7c67
khudaynazrovs <khudaynazrovsd@gmail.com>
2024-03-24 11:27:00 +0900

    abs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './navbar';
-
+import RouterComponent from './router';
 
 
 
---
+++ src/router.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowseRouter,Routes,Route} from "react-router-dom";
+
+import Navbar from './navbar';
+import HomeComponent from './home';
+
+const RouterComponent = () => {
+  return (
+    <BrowseRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomeComponent/>}/>
+    </Routes>
+    </BrowseRouter>
+  )
+}
+
+export default Router
